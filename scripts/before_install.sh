1e12f056446fd3b8ab7d3624f68e9882e0e17bcc
Mhuammad Hamza Shabbir <hamzaarain852sba@gmail.com>
2023-12-18 17:42:55 +0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,10 +4,6 @@
 sudo su
 
 # Update package information
-apt update
+apt update python3  python3.10-venv -y
 
-# Install Python 3
-apt install -y python3
 
-# Install Python 3.10 virtual environment
-apt install -y python3.10-venv
